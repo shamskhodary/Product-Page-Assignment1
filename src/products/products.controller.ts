305c2b3fbc18e1edb0f7d7b51c1773f101609e46
shamskhodary <shams.khodary@gmail.com>
2023-01-22 13:59:25 +0200

    filtered products by category and price relates #9

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,7 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { Query } from '@nestjs/common/decorators';
+import { QueryProductDto } from './dto/product.dto';
+import { Product } from './entities/product.entity';
 import { ProductsService } from './products.service';
 
 @Controller('products')
 export class ProductsController {
   constructor(private productService: ProductsService) {}
+
+  @Get()
+  async findAll(@Query() dto?: QueryProductDto): Promise<Product[]> {
+    const allProducts = await this.productService.findAll(
+      dto.category,
+      dto.sort,
+    );
+
+    return allProducts;
+  }
 }
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,3 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/sequelize';
+import { Product } from './entities/product.entity';
 @Injectable()
-export class ProductsService {}
+export class ProductsService {
+  constructor(@InjectModel(Product) private productModel: typeof Product) {}
+
+  async findAll(category?: string, sort?: string): Promise<Product[]> {
+    const whereObj = {};
+    let order: any = [];
+
+    if (category) {
+      whereObj['category'] = category;
+    }
+
+    if (sort === 'lowest') {
+      order = [['price', 'ASC']];
+    } else if (sort === 'highest') {
+      order = [['price', 'DESC']];
+    }
+
+    const products = await this.productModel.findAll({
+      where: whereObj,
+      order,
+    });
+
+    return products;
+  }
+}
